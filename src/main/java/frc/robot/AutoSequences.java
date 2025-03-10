c69a12cce8d07459d09c6c710f31430462678374
Falsehook4716 <147114175+Falsehook4716@users.noreply.github.com>
2025-03-10 08:33:23 -0700

    stud

--- src/main/java/frc/robot/AutoSequences.java
+++ src/main/java/frc/robot/AutoSequences.java
@@ -4,14 +4,8 @@ package frc.robot;
 import java.util.ArrayList;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.DriverDisplay;
-import frc.robot.subsystems.GamePieceDetector;
 import frc.robot.subsystems.PositionEstimator;
-import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.utilityObjects.Vector2D;
 
 public class AutoSequences {
   public static int[] coralList;
@@ -26,10 +20,10 @@ public class AutoSequences {
     succesfulShots = 0;
     Constants.gyro.setYaw(DriverDisplay.angleToAssign);
     PositionEstimator.realCoralList = new ArrayList<>();
-    for (int i = 0; i < coralList.length; i++) 
+    for (int i = 0; i < coralList.length; i++)
     {
         //PositionEstimator.realCoralList.add(Constants.allCoralsPos[coralList[i]-1]);
-      
+
     }
   }
 
@@ -78,10 +72,10 @@ public class AutoSequences {
   // shoot basic, drive out
     public static void autoSequence5() {
         ClimberSubsystem.moveClimbers(-1, 0);
-        if (isAutoTimeBetween(0, 2)) 
+        if (isAutoTimeBetween(0, 2))
         {
           ArmSubsystem.manualMoveArmTo();
-          ArmSubsystem.SpinShooter(1); 
+          ArmSubsystem.SpinShooter(1);
         }
         else if (isAutoTimeBetween(2, 3)) {
             SwerveSubsystem.DriveDriverOriented(0, 0, 0);
@@ -93,7 +87,7 @@ public class AutoSequences {
           ArmSubsystem.SpinShooter(0);
           ArmSubsystem.rotateArm(0);
             SwerveSubsystem.DriveDriverOriented(0, 0.25, 0);
-        } else Functions.killAllMotors(); 
+        } else Functions.killAllMotors();
     }
 
   // drive out, shoot aimbot
@@ -106,7 +100,7 @@ public class AutoSequences {
         ArmSubsystem.PrepShooter(Constants.defaultShooterSpeed);
         ArmSubsystem.ShootSpeaker();
     } else Functions.killAllMotors();
-    
+
     }
 
   // Retract Climbers, Shoot Basic, Collect Nearest, Shoot
@@ -137,12 +131,12 @@ public class AutoSequences {
           hadCoral = false;
           if (GamePieceDetector.coralVisible && GamePieceDetector.coralDist < 3) {
             ArmSubsystem.IntakeRing();
-            SwerveSubsystem.CollectCoral(0, 0, 0.5);        
+            SwerveSubsystem.CollectCoral(0, 0, 0.5);
           } else {
             wasInDriveToCoral = true;
             Vector2D closestCoral = Constants.allCoralsPos[PositionEstimator.nearestAutoCoral()];
             if (SwerveSubsystem.atTargetAngle) SwerveSubsystem.DriveTo(closestCoral.x, closestCoral.y, PositionEstimator.angleToClosestCoral()+180, 0.5, 0.5, 0, 0);
-            else SwerveSubsystem.DriveDriverOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5); 
+            else SwerveSubsystem.DriveDriverOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5);
           }
         }
       } else {
@@ -165,7 +159,7 @@ public class AutoSequences {
             SwerveSubsystem.DriveTo((Robot.isRedAlliance ? Constants.redSpeaker : Constants.blueSpeaker).x, (Robot.isRedAlliance ? Constants.redSpeaker : Constants.blueSpeaker).y, PositionEstimator.angleToSpeaker(), 0.5, 0.5, 0, 0);
             ArmSubsystem.SpinIntake(0);
           }
-          
+
         } else {
           if (hadCoral) {
             succesfulShots++;
@@ -175,18 +169,18 @@ public class AutoSequences {
           hadCoral = false;
           ArmSubsystem.IntakeRing();
           if (GamePieceDetector.coralVisible && GamePieceDetector.coralDist < 3) {
-            SwerveSubsystem.CollectCoral(0, 0, 0.5);        
+            SwerveSubsystem.CollectCoral(0, 0, 0.5);
           } else {
             wasInDriveToCoral = true;
             Vector2D closestCoral = PositionEstimator.realCoralList.get(PositionEstimator.nearestAutoCoral());
             if (SwerveSubsystem.atTargetAngle) SwerveSubsystem.DriveTo(closestCoral.x, closestCoral.y, PositionEstimator.angleToClosestCoral()+180, 0.5, 0.5, 0, 0);
-            else SwerveSubsystem.DriveDriverOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5); 
+            else SwerveSubsystem.DriveDriverOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5);
           }
         }
       } else {
         Functions.killAllMotors();
       }
-    
+
   }
 
   // Retract Climbers, Shoot Basic, Collect and Shoot as Many as Possible
@@ -206,7 +200,7 @@ public class AutoSequences {
             } else if (succesfulShots == 1) {
               SwerveSubsystem.DriveTo((Robot.isRedAlliance ? Constants.redSpeaker : Constants.blueSpeaker).x, (Robot.isRedAlliance ? Constants.redSpeaker : Constants.blueSpeaker).y, PositionEstimator.angleToSpeaker(), 0.5, 0.5, 0, 0);
               ArmSubsystem.SpinIntake(0);
-            }         
+            }
           }
 
         } else {
@@ -218,12 +212,12 @@ public class AutoSequences {
           hadCoral = false;
           if (GamePieceDetector.coralVisible && GamePieceDetector.coralDist < 3) {
             ArmSubsystem.IntakeRing();
-            SwerveSubsystem.CollectCoral(0, 0, 0.5);        
+            SwerveSubsystem.CollectCoral(0, 0, 0.5);
           } else {
             wasInDriveToCoral = true;
             Vector2D closestCoral = Constants.allCoralsPos[PositionEstimator.nearestAutoCoral()];
             if (SwerveSubsystem.atTargetAngle) SwerveSubsystem.DriveTo(closestCoral.x, closestCoral.y, PositionEstimator.angleToClosestCoral()+180, 0.5, 0.5, 0, 0);
-            else SwerveSubsystem.DriveDriverOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5); 
+            else SwerveSubsystem.DriveDriverOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5);
           }
         }
       } else {
@@ -248,7 +242,7 @@ public class AutoSequences {
             SwerveSubsystem.DriveTo((Robot.isRedAlliance ? Constants.redSpeaker : Constants.blueSpeaker).x, (Robot.isRedAlliance ? Constants.redSpeaker : Constants.blueSpeaker).y, PositionEstimator.angleToSpeaker(), 0.8, 0.5, 0, 0);
             ArmSubsystem.SpinIntake(0);
           }
-          
+
         } else {
           if (hadCoral) {
             succesfulShots++;
@@ -262,25 +256,25 @@ public class AutoSequences {
           ArmSubsystem.IntakeRing();
           if (GamePieceDetector.coralVisible && GamePieceDetector.coralDist < 2) {
             wasInDriveToCoral = true;
-            SwerveSubsystem.CollectCoral(0, 0, 0.8);        
+            SwerveSubsystem.CollectCoral(0, 0, 0.8);
           } else {
             Vector2D closestCoral = PositionEstimator.realCoralList.get(PositionEstimator.nearestAutoCoral());
             if (Math.abs(Functions.DeltaAngleDeg(PositionEstimator.robotPosition.getRotation().getDegrees(), PositionEstimator.angleToClosestCoral()+180)) < 10) SwerveSubsystem.DriveTo(closestCoral.x, closestCoral.y, PositionEstimator.angleToClosestCoral()+180, 0.8, 0.5, 0, 0);
-            else SwerveSubsystem.DriveFieldOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5); 
+            else SwerveSubsystem.DriveFieldOrientedAtAngle(0, 0, PositionEstimator.angleToClosestCoral()+180, 0.5);
           }
         }
       } else {
         Functions.killAllMotors();
       }
-    
+
   }*/
 
 
 
   public static boolean isAutoTimeBetween(double timeMin, double timeMax) {
-    return timeMin < Timer.getFPGATimestamp() - Constants.timeSinceStartAtAutoStart && timeMax > Timer.getFPGATimestamp() - 
+    return timeMin < Timer.getFPGATimestamp() - Constants.timeSinceStartAtAutoStart && timeMax > Timer.getFPGATimestamp() -
     Constants.timeSinceStartAtAutoStart ;
   }
-  
+
 }
- 
+
